568b96f703ebd1b364abf7988a1bfdea6ada228d
dottjt <julius_dott@hotmail.com>
2017-06-17 11:11:23 +1000

    sitemap added to website

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@
 
   // *** dependencies *** //
   const express = require('express');
-  const sitemap = require('express-sitemap');
+  // const sitemap = require('express-sitemap');
 
   const appConfig = require('./config/main-config.js');
   const routeConfig = require('./config/route-config.js');
@@ -18,45 +18,45 @@
   routeConfig.init(app);
   errorConfig.init(app);
 
-sitemap({
-    sitemap: 'server/views/sitemap.xml', // path for .XMLtoFile
-    robots: 'server/views/robots.txt', // path for .TXTtoFile
-    generate: app, // option or function, is the same
-    sitemapSubmission: 'server/views/sitemap.xml', // path of sitemap into robots
-    route: { // specific option for some route
-      '/': {
-        changefreq: 'always',
-        priority: 1.0,
-      },
-      '/about': {
-        changefreq: 'always',
-        priority: 1.0,
-      },
-      '/login': {
-        changefreq: 'always',
-        priority: 1.0,
-      },
-      '/signup': {
-        changefreq: 'always',
-        priority: 1.0,
-      },
-      '/register': {
-        changefreq: 'always',
-        priority: 1.0,
-      },
-      '/contact': {
-        changefreq: 'always',
-        priority: 1.0,
-      },
-      '/admin': {
-        disallow: true, // write this route to robots.txt
-      },
-      '/nooo': {
-        lastmod: '2014-06-20',
-        changefreq: 'never',
-      }
-    },
-  }).toFile(); // write sitemap.xml and robots.txt
+// sitemap({
+//     sitemap: 'server/views/sitemap.xml', // path for .XMLtoFile
+//     robots: 'server/views/robots.txt', // path for .TXTtoFile
+//     generate: app, // option or function, is the same
+//     sitemapSubmission: 'server/views/sitemap.xml', // path of sitemap into robots
+//     route: { // specific option for some route
+//       '/': {
+//         changefreq: 'always',
+//         priority: 1.0,
+//       },
+//       '/about': {
+//         changefreq: 'always',
+//         priority: 1.0,
+//       },
+//       '/login': {
+//         changefreq: 'always',
+//         priority: 1.0,
+//       },
+//       '/signup': {
+//         changefreq: 'always',
+//         priority: 1.0,
+//       },
+//       '/register': {
+//         changefreq: 'always',
+//         priority: 1.0,
+//       },
+//       '/contact': {
+//         changefreq: 'always',
+//         priority: 1.0,
+//       },
+//       '/admin': {
+//         disallow: true, // write this route to robots.txt
+//       },
+//       '/nooo': {
+//         lastmod: '2014-06-20',
+//         changefreq: 'never',
+//       }
+//     },
+//   }).toFile(); // write sitemap.xml and robots.txt
 
 // app.get('/sitemap.xml', function(req, res) { // send XML map
 
